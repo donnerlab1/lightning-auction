30f507ee06dfb7f78b73bd4ec6376b1cb522e589
kon <kon@kon.ninja>
2020-04-01 12:03:11 +0200

    add new create advertiser UX

--- LightningAuction.Client/wwwroot/js/stuff.js
+++ LightningAuction.Client/wwwroot/js/stuff.js
@@ -8,8 +8,17 @@ function postRequest() {
     xhr.onload = function () {
         console.log(this.response)
         console.log(this.responseText);
+        if (this.status == 200) {
+            var obj = JSON.parse(this.responseText)
 
-        document.getElementById("response").innerHTML = "response: " + this.status+";"+ this.responseText;
+            document.getElementById("response").innerHTML = "Pay this invoice: " + obj.invoice
+        } else if (this.status == 400) {
+            var obj = JSON.parse(this.responseText)
+            document.getElementById("response").innerHTML = "ERROR: "+ obj.error
+        } else {
+            document.getElementById("response").innerHTML = "response: " + this.status + ";" + this.responseText;
+        }
+        
     }
 
     xhr.open("post", "http://localhost:8012/ads/upload");
